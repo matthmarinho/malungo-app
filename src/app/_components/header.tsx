543b226b0ad0fc279fd9ba981ff906e4d9a12ee5
Matthaeus <matthaeusbm@gmail.com>
2024-08-10 03:52:57 -0300

    feat: ensure barbershops can be searched by title and service

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -4,12 +4,15 @@ import SidebarSheet from "./sidebar-sheet";
 import { MenuIcon } from "lucide-react";
 import { Sheet, SheetTrigger } from "./ui/sheet";
 import { Button } from "./ui/button";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <Card>
       <CardContent className="p-5 flex flex-row items-center justify-between">
-        <Image alt="Malungo App" src="/logo.png" height={18} width={120} />
+        <Link href="/">
+          <Image alt="Malungo App" src="/logo.png" height={18} width={120} />
+        </Link>
         <Sheet>
           <SheetTrigger asChild>
             <Button size="icon" variant="outline">
