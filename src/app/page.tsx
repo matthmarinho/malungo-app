1e085653a4b9a8a46286b0f3b79dd938fe9672a8
Matthaeus <matthaeusbm@gmail.com>
2024-08-14 04:56:41 -0300

    feat: update welcome message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,8 @@ import Search from "./_components/search";
 import Link from "next/link";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./_lib/auth";
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
 
 const Home = async () => {
   const session = await getServerSession(authOptions);
@@ -44,8 +46,18 @@ const Home = async () => {
       <Header />
 
       <div className="p-5">
-        <h2 className="text-xl font-bold">Olá, Milena!</h2>
-        <p>Quarta-feira, 07 de agosto</p>
+        <h2 className="text-xl font-bold">
+          Olá, {session?.user ? session.user.name : "bem vindo"}!
+        </h2>
+        <p>
+          <span className="capitalize">
+            {format(new Date(), "EEEE, dd", { locale: ptBR })}
+          </span>
+          <span>&nbsp;de&nbsp;</span>
+          <span className="capitalize">
+            {format(new Date(), "MMMM", { locale: ptBR })}
+          </span>
+        </p>
 
         <div className="mt-6">
           <Search />
